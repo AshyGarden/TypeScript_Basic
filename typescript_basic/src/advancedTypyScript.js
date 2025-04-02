e51db1a714a8b1d53c0eb4969ec1767bab84a2fe
jun940317 <jun940317@gmail.com>
2025-04-02 17:48:51 +0900

    250402 modified advanced tsSkills js

--- typescript_basic/src/advancedTypyScript.js
+++ typescript_basic/src/advancedTypyScript.js
@@ -1,1 +1,132 @@
 "use strict";
+//Typescript高級技法
+//computerタイプなのでcoreタイプに代入可能
+const comToCore = {
+    parts: "cooler"
+};
+//coreタイプなのでcomputerタイプに代入可能
+const coreToCom = {
+    spec: "i11900K"
+};
+const book1 = {
+    species: "ITBook1",
+    bookNum: "101"
+};
+const book2 = {
+    name: "TypeScript入門",
+    bookNum: 102
+};
+//下記の場合 b1 のタイプは string |number
+function showBook(book) {
+    const b1 = book.bookNum; //エラーが発生しない！
+    console.log(book);
+}
+function useUnion(union) {
+    const result = union("union");
+    console.log(result); //このとき、result のタイプは string |number
+}
+function useOpChn(oc) {
+    const opChn = oc?.num;
+    console.log(opChn); //number | undefined 
+    // optionalChaning値が存在しない場合、number
+    // optional Chaning値が存在しない場合 undefined
+}
+function useOpChn1(getOpChn) {
+    const opChn1 = getOpChn?.(); //()前に。を入れる
+}
+const lit1 = "literal1";
+// const liter1:literal1 = "liter1"; <-error
+//種類 リテラル タイプ
+const strLit = "literal1"; //文字列リテラルタイプ
+const one = 1; // 数字リテラルタイプ
+const trueLit = true; // ブーリアンリテラルタイプ
+const three = 3n; // Bigint リテラル タイプ
+// テンプレートリテラルタイプ - `(バックティック)で包んだ文字列タイプの一種
+// 関数の戻り値タイプでテンプレートリテラルタイプを使用->戻り値である文字列を検査可能
+function templateLiteral() {
+    const rand = Math.random();
+    if (rand > 0.5) {
+        return "Template Literal 0.5 Over"; //${string} = Literal 0.5 Over
+    }
+    else {
+        return "Template Literal 0.5 Under"; //${string} = Literal 0.5 Under
+    }
+}
+//ユニオンタイプ+リテラルタイプ
+//特定値の一部だけを受け取りたい場合に有用だ
+function plusMinus(type) {
+    return type === "plus" ? 1 : -1;
+}
+console.log(plusMinus("plus"));
+console.log(plusMinus("minus"));
+// console.log(plusMinus("zero")); //error(該当しない値)
+//タイプを広げる(widening)
+//let - 変数がリテラルタイプと推論される場合、原始タイプとして返還される！
+// オブジェクトリテラルタイプが推論されたとき、各プロパティがリテラルタイプになると広がりが起こる
+const widen1 = "widen";
+let widen2 = "widen";
+const mugCup = {
+    name: "MugCup",
+    price: 10000
+};
+function plusMinus2(type) {
+    return type === "plus" ? 1 : -1;
+}
+function plusMinusNone(num, type) {
+    if (type === "none") {
+        return 0;
+    }
+    else {
+        return num * plusMinus2(type);
+    }
+}
+console.log(plusMinusNone(4, "plus")); //4
+console.log(plusMinusNone(5, "minus")); //-5
+console.log(plusMinusNone(3, "none")); //0
+// タイプ絞り込み 2. type of 演算子を活用した絞り込み
+// 文字列                    -"string"
+// 数字                      -"number"
+// ブーリアン                 - "boolean"
+// BigInt                    -"bigint"
+// シンボル                   - "symbol"
+// null                      -"object"！
+// undefined                 -"undefined"
+// オブジェクト(関数を除く)    - "object"
+// 関数                       - "function"
+console.log(typeof "hello"); //string
+console.log(typeof {}); //object
+function lookup1Setting(lookup, name) {
+    return {
+        ...lookup,
+        name
+    };
+}
+let key = "name";
+key = "num"; //letにより変更可能
+//any - タイプ検査を無効化するタイプ
+// タイプスクリプトの検査よりコンパイリング性能向上のために主に使用
+//何を入れてもコンパイルエラーが発生しない->ほとんどのランタイム時エラー発生
+//自動入力のサポートを受けられない
+function anyThing(obj) {
+    console.log(obj.useUnion.name); //希望するプロパティにアクセス可能
+    obj(); // 関数呼び出し 可能
+    const result = obj + 10; // 計算可能
+    return result;
+}
+//unknown - 何でも入れられるタイプ
+//何でも入れることができるので性能制限がひどい！
+//プロパティへのアクセスが非常に制限的 -> コンパイルエラーが頻繁に発生
+function unknownThing(val) {
+    console.log(val);
+}
+unknownThing(3);
+unknownThing({
+    obj1: {
+        name1: "unknown"
+    }
+});
+unknownThing(() => {
+    console.log("id");
+});
+//object - 原始値以外のすべて
+//never - 適切な値が存在しない。
--- typescript_basic/src/class.js
+++ typescript_basic/src/class.js
@@ -1,1 +1,240 @@
-"use strict";
+//class
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
+};
+var _unknownProperty_age, _typeProperty_age, _child3_age;
+import { error } from "console";
+//class宣言 - json形式と類似
+class userClass {
+    constructor() {
+        this.name = "user1";
+        this.age = 30;
+    }
+}
+const u1 = new userClass();
+//プロパティ宣言
+console.log(u1.name); //user1
+console.log(u1.age); //30
+//userをclassオブジェクトにして使用すること
+const upperUserClass = {
+    uuc: userClass
+};
+const upperU1 = new upperUserClass.uuc();
+console.log(upperU1.age); //30
+// メソッド宣言 - クラス内にマサード宣言も入れることができます。 
+// この時、当該クラスは自動的にそのマッサードを持った状態で作られます
+// メソッド宣言構文は、オブジェクトリテラルのマサード手法と同じです
+class method1 {
+    constructor() {
+        this.m1 = "";
+        this.m2 = 0;
+    }
+    isTrue() {
+        return true;
+    }
+    setM2(m2) {
+        this.m2 = m2;
+    }
+}
+const methodUser1 = new method1();
+console.log(methodUser1.isTrue()); //true
+console.log(methodUser1.setM2(30)); //30セット
+//生成者 - newでインスタンスが作成されたときに呼び出される関数
+//作成者は自分の読み取り専用プロパティにも値を代入できます！
+class constructor1 {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    isAdult() {
+        return this.age >= 20;
+    }
+}
+const c1 = new constructor1("c1", 25);
+console.log(c1.name); //c1
+console.log(c1.isAdult()); //true
+//静的プロパティ/静的メソッド - 通常のプロパティ/メソッド宣言の前にstaticを付けて宣言します
+//クラス自体に属するプロパティ+マサードに変更されます
+class staticFunc1 {
+    static getStatic1() {
+        return new staticFunc1(staticFunc1.s1, 32);
+    }
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    isAdult() {
+        return this.age >= 20;
+    }
+}
+staticFunc1.s1 = "s1"; //静的変数
+console.log(staticFunc1.s1); //s1 - 関数コールと呼ぶべきです
+const static1 = new staticFunc1("s2", 35);
+console.log(static1.age); //35
+//アクセス制限者
+//public:何も貼らない場合
+//private: class 内部からのみアクセス可能
+//protected:自分と相続したクラスでのみアクセス可能
+class restrictedAccess {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    isAdult() {
+        return this.age >= 20;
+    }
+}
+//生成者引数におけるプロパティ宣言
+class simpleCsrt {
+    // name: string ;
+    // age: number ; 
+    // constructor(name: string, age:number){
+    //     this.name = name;
+    //     this.age = age;
+    // }
+    //上記の内容と同じです。
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    } //一行で要約して生成者生成可能
+}
+//クラス表現式でクラス作り
+const classExpress = class {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+};
+//非公開プロパティ
+class unknownProperty {
+    constructor(name, age) {
+        _unknownProperty_age.set(this, void 0); //非公開プロパティ = privateと同じ機能 + json.stringfy でも値が出力されない!(注意)
+        this.name = name;
+        __classPrivateFieldSet(this, _unknownProperty_age, age, "f");
+    }
+}
+_unknownProperty_age = new WeakMap();
+//クラスの静的初期化ブロック - クラス宣言評価途中に動作+複数使用 可能！
+class staticBlock {
+}
+(() => {
+    console.log("Static initialized!");
+})();
+//タイプ引数を持つクラス - クラスはタイプ引数を持つことができます。
+class typeProperty {
+    constructor(name, age, data) {
+        _typeProperty_age.set(this, void 0);
+        this.name = name;
+        __classPrivateFieldSet(this, _typeProperty_age, age, "f");
+        this.data = data;
+    }
+}
+_typeProperty_age = new WeakMap();
+const typeProp = new typeProperty("typeP2", 20, "Added");
+const dataProp = typeProp.data; //この場合 dataは string タイプ
+const typeProp2 = new typeProperty("typeP2", 30, { num: 123 });
+const dataProp2 = typeProp2.data; //この場合 dataは number タイプ
+//--------------------------------------------------------------------------
+//クラスタイプ
+//クラスの宣言はインスタンスタイプを作成します。- クラス オブジェクトという値を作成すると同時に、インスタンス タイプを宣言します。
+//newシグネチャーを利用してインスタンス化可視性表現
+class newSignature {
+    constructor() {
+        this.name = "class1";
+        this.age = 2020;
+    }
+}
+const newSignatureEx = newSignature;
+const sig1 = new newSignatureEx();
+console.log(sig1.name, sig1.name);
+//クラス相続
+//子供は親の機能を引き継ぎます
+class parent1 {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+class Child1 extends parent1 {
+    constructor() {
+        super(...arguments);
+        this.rank = 1;
+    }
+}
+//相続した親の価値+自分の変数の価値
+const p1 = new Child1("p1", 30);
+console.log(p1.name); //p1
+console.log(p1.age); //30
+console.log(p1.rank); //1
+//親機能上書き(override)
+class parent2 {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    isAdult() {
+        return this.age >= 20;
+    }
+}
+class Child2 extends parent1 {
+    constructor() {
+        super(...arguments);
+        this.rank = 1;
+    }
+    isAdult() {
+        return false;
+    }
+}
+const p21 = new parent2("p2", 25);
+const p22 = new Child2("p2", 25);
+console.log(p21.isAdult); //true
+console.log(p22.isAdult); //false
+class child3 {
+    constructor(name, age) {
+        _child3_age.set(this, void 0);
+        this.name = name;
+        __classPrivateFieldSet(this, _child3_age, age, "f");
+    }
+}
+_child3_age = new WeakMap();
+//---------------------------------------------------------------------------
+//this
+//矢印関数
+//矢印関数は自分自身のthisを持ちません！
+//そのため、thisの外側から関数を受け取ります。
+class arrowFunc {
+    constructor(a1, a2) {
+        this.a1 = a1;
+        this.a2 = a2;
+    }
+    arrowFilter(arrowFuncs) {
+        return arrowFuncs.filter(u => u.a2 > this.a2);
+    }
+}
+//関数内外this
+//プログラムの一番外側のthis= undefined
+//クラス宣言内のプロパティ宣言= 一般的なnew生成のインスタンスを指定します
+//静的プロパティの初期化式 + static 内部の初期化式 this = クラスオブジェクトそのものを指します
+//例外処理
+//Throw文 / Errorオブジェクト / try-catch文
+try {
+    console.log("try");
+    throw error; //エラー発生時
+}
+catch (err) {
+    console.log("Error!"); //エラー構文実行
+}
+//例外処理+全域脱出+finally
+try {
+    console.log("try");
+    throw error;
+}
+catch (err) {
+    console.log("Error!");
+}
+finally {
+    console.log("finally!"); //すべての処理が終了するか、エラー処理後に実行します
+}
